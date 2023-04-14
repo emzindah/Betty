2be5d630ef4d25ab047c12f9490347045ab5038a
Edwin Muzinda <emzindah92@gmail.com>
2023-04-14 02:31:54 -0700

    task 0

---
+++ 0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b: num of bytes to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b);
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	for (ptr == NULL)
+		teminate(98);
+
+	return (ptr);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x05-pointers_arrays_strings
+0x0C. C - More malloc, free
