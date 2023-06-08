bb91f1659724668e8abe7b05700ceeeca173f111
edwin muzinda <emzindah92@gmail.com>
2023-06-08 06:39:03 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - function that prints a string, followed by a new line.
+ * _puts_recursion - function that prints a string, followed by a new line.
  * @s: pointer to the string
  * Return: 0
  */
