bac3b7e9fc217973cb316261ec803a16c4c789c1
edwin muzinda <emzindah92@gmail.com>
2023-06-08 06:23:10 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,17 @@
 /**
  * main - function that prints a string, followed by a new line.
  * @s: pointer to the string
- *
+ * Return: 0
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s == ('\0'))
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar (*s);
-	_puts_recursion(s + 1);
+	s++;
+	_puts_recursion(s);
 }
 
