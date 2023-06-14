b98c148f9ba70b15a89b8d5ce1f6b53232fb2ec2
edwin muzinda <emzindah92@gmail.com>
2023-06-14 08:29:34 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
 #include "main.h"
+#include<stdlib.h>
+#include<ctype.h>
 /**
- * main -
+ * main - adds poditive numbers & printing the result,
+ * followed by a new line
+ * @argv: number of arguments
+ * @argc: array of arguments
+ * Return: 0 on success , 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int add = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add = add + atoi(argv[a]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
