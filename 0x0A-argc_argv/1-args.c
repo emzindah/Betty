8c766b10ae2a8ae710401f32818dbf4e391defd6
edwin muzinda <emzindah92@gmail.com>
2023-06-14 02:17:41 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,11 +5,11 @@
  * main - prints a number of arguments passed into it
  * @argc: count arguments
  * @argv: ignore arguments
- * Return: 0 always
+ * Return: 0 always (success)
  */
 int main(int argc, char *argv[])
 {
-	(void) argv;
-	printf("%d/n", argc - 1);
+	(void) argv; /*ignore argv*/
+	printf("argv = %d/n", argc);
 	return (0);
 }
