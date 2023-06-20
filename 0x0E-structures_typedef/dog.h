5194e8acadb5d8a981ddbeefb9129d8a8ebe1795
edwin muzinda <emzindah92@gmail.com>
2023-06-20 08:39:26 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - dogs infomation
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: dog's discription
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
