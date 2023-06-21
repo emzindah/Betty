0c095929edb059512ba825fbeb318a452bf5d830
edwin muzinda <emzindah92@gmail.com>
2023-06-21 04:33:23 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,10 @@ void print_dog(struct dog *d)
 		;
 	}
 
-	if ((*d).name == NULL)
-		(*d).name = "(nil)";
-	if ((*d).owner == NULL)
-		(*d).owner = "(nil)";
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
 
 	printf("Name: %s\n", d->name);
