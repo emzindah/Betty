97039a7b589913da96ba738510d70b6d6f455aae
emzindah <emzindah92@gmail.com>
2023-08-08 15:03:25 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,51 +3,45 @@
 #include <stdlib.h>
 
 /**
- * get_str_len - length of a string
- * @str: length of string to be found
- * Return: integer value of the string
- */
-int get_str_len(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	return (i + 1);
-}
-
-/**
  * str_concat - function that concatenates two strings
- * @s1: first input
- * @s2: second input
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
  *Return: contents of s1 and s2
- *function should return NULL on failure
- *if NULL is passed, treat it as an empty string
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int s1_size, s2_size, i, j;
+	int n, m;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	s1_size = get_str_len(s1);
-	s2_size = get_str_len(s2);
+	n = 0;
+	m = 0;
 
-	ptr = (char *) malloc(((s1_size + s2_size) - 1) * sizeof(char));
+	while (s1[n] != '\0')
+		n++;
+	while (s2[m] != '\0')
+		m++;
+	ptr = malloc(sizeof(char) * (n + m + 1));
 
 	if (ptr == NULL)
 		return (NULL);
+	n = m = 0;
+	while (s1[n] != '\0')
+	{
+		ptr[n] = s1[n];
+		n++;
+	}
 
-	for (i = 0; s1[i] != '\0'; i++)
-		ptr[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; j++)
-		ptr[i] = s2[j];
-	ptr[i] = '\0';
-
-	return (ptr);
+	while (s2[m] != '\0')
+	{
+		ptr[n] = s2[m];
+		m++;
+		n++;
+	}
+	ptr[n] = '\0';
+		return (ptr);
 }
