97a49c2125e7c806928bef298a6d993a6da352aa
emzindah <emzindah92@gmail.com>
2023-08-11 06:04:45 -0700

    task 4 realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,7 +4,7 @@
 /**
  * _realloc - reallocates memory using malloc & free
  * @old_size: size allocated previously by malloc
- * @new_size: new size allocated by _realloc
+ * @new_size: new size allocated 
  * @ptr:  pointer to the memory previously allocated
  *
  * Return: a newly allocated pointer to memory
@@ -12,7 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new;
-	unsigned int a, b;
+	unsigned int a, b = new_size;
 	char *old = ptr;
 
 	if (new_size == old_size)
